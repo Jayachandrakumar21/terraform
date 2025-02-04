61d2832f359eb60bdc61266a87417d571f9cd959
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-02-04 21:20:57 +0530

    terraform

--- count/ec2.tf
+++ count/ec2.tf
@@ -31,3 +31,13 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
 }
+
+/* resource "aws_security_group_rule" "example" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.example.cidr_block]
+  ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
+  security_group_id = [aws_security_group.allow_tls.id]
+} */
