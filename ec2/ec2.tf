46b122e5e8c6afd37bbb6435f41e49cea7bffcfb
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-04-24 11:13:10 +0530

    t3.small to t2.micro

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "server" {
   instance_type          = "t2.micro"
 
   tags = {
-    Name = "terraform-demo"
+    # Name = "docker-demo"
+    Name = "jenkins"
   }
 }
 
@@ -13,9 +14,9 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic and all outbound traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
